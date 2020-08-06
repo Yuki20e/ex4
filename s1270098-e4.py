6dd8ff8c743e341e75882c7b6555139c21353702
Yuki20e <yu.tanioka0526@gmail.com>
2020-08-06 16:50:48 +0900

    User name added

--- s1270098-e4.py
+++ s1270098-e4.py
@@ -1,15 +1,3 @@
-import random
-
-l = ["Heads", "Tails"]
-head = 0
-tail = 0
-
-print("Tossing a coin...")
-for i in range(3):
-    j = random.choice(l)
-    print("Round {}: {}".format(i+1,j))
-    if j == "Heads":
-        head += 1
-    else:
-        tail += 1
-print("Heads: {}, Tails: {}".format(head,tail))
+print('Who are you?')
+name = input('> ')
+print('Hello, ' + name + '!')
